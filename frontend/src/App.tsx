6ab795869343039fc00f0e96458642db5e8d7a79
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2021-12-09 13:43:19 +0100

    Add default footer

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import './assets/styles/main.css'
+import './assets/styles/responsive.css'
+import Footer from './modules/layout/footer'
+
+function App() {
+  return (
+    <div>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
