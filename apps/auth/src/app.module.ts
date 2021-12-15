e48c74d95f8a075565e6c5a345826f6287e1d678
AlexandrePhilibertCPNV <alexandre.philibert@cpnv.ch>
2021-12-15 11:14:55 +0100

    Add typeorm database connection

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      // This is the name of the service inside the docker-compose file, we
+      // should load the module config from env variables.
+      host: 'auth-db',
+      port: 5432,
+      username: 'auth',
+      password: 'auth',
+      database: 'auth',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
