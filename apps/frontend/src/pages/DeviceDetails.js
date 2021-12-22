7448af04a824a9b63be98b3d940005d9ae2d9e51
SuperQuentin <quentin.aellen@cpnv.ch>
2021-12-22 14:36:21 +0100

    Add display of devices details edit form

--- apps/frontend/src/pages/DeviceDetails.js
+++ apps/frontend/src/pages/DeviceDetails.js
@@ -1,13 +1,29 @@
+import { Row, Col, Card, Switch, Input } from "antd";
 
-import { Row, Col } from "antd";
+const { TextArea } = Input;
 
 export default function DeviceDetails(props) {
     return (
         <>
             <Row gutter={[24, 0]}>
-                <Col xs="24" xl={24}>
-                    <h2>Device {props.match.params.id}</h2>
-                </Col>
+                <Col span={24} md={8} className="mb-24 ">
+                    <Card
+                        bordered={false}
+                        className="header-solid h-full"
+                        title={<h3 className="font-semibold m-0">Informations</h3>}
+                    >
+                        <ul className="list settings-list">
+                            <li>
+                                <label>{<h4 className="font-semibold m-0">Device name</h4>}</label>
+                                <Input  defaultValue={""} placeholder={"device name"}/>
+                            </li>
+                            <li>
+                                <label>{<h4 className="font-semibold m-0">Description</h4>}</label>
+                                <TextArea showCount maxLength={255} placeholder={"description"} defaultValue={""}/>
+                            </li>
+                        </ul>
+                    </Card>
+                </Col> 
             </Row>
         </>
     )
