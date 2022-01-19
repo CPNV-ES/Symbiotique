9f77bcdb776e8e7146b573d7b7c6bc19396dad26
AlexandrePhilibertCPNV <alexandre.philibert@cpnv.ch>
2022-01-19 11:55:53 +0100

    Authenticate device against the auth-devices

--- apps/broker/src/index.js
+++ apps/broker/src/index.js
@@ -6,6 +6,20 @@ aedes.authenticate = async (client, username, password, callback) => {
   console.log(`client ${client.id} sent an authentication request`);
 
   try {
+    await axios.post("http://devices-auth:3000/auth/sign-in", {
+      username,
+      password,
+    });
+  } catch (e) {
+    await axios.post("http://devices:3000/devices", {
+      clientId: client.id,
+      state: "AUTHENTICATION_FAILED",
+    });
+
+    return callback(null, false);
+  }
+
+  try {
     await axios.get(`http://devices:3000/devices/${client.id}`);
   } catch (e) {
     if (e.response.status === 404) {
