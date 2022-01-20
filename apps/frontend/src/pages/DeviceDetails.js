87bb3a388ffe18bed94180a67ad0da33376fd97b
SuperQuentin <quentin.aellen@cpnv.ch>
2022-01-20 11:08:09 +0100

    Add device detail with data query

--- apps/frontend/src/pages/DeviceDetails.js
+++ apps/frontend/src/pages/DeviceDetails.js
@@ -1,8 +1,29 @@
-import { Row, Col, Card, Switch, Input } from "antd";
+import { Row, Col, Card, Input, Select, Spin } from "antd";
+import { strictEqual } from "assert";
+import {  useState } from "react";
+
+import { useDevice } from "../hooks/useDevice";
+
+const DeviceType = [
+    'TEMPERATURE',
+    'HUMIDITY',
+    'LIGHT',
+    'PRESSURE',
+    'CO2',
+    'DOOR',
+]
 
 const { TextArea } = Input;
 
+function handleTypeDropdownButtonClick(e) {
+    console.log('click left button', e);
+}
+
 export default function DeviceDetails(props) {
+    const [deviceId, setDeviceId] = useState(props.match.params.id);
+
+    const { status, data, error, isFetching } = useDevice(deviceId);
+
     return (
         <>
             <Row gutter={[24, 0]}>
@@ -12,18 +33,32 @@ export default function DeviceDetails(props) {
                         className="header-solid h-full"
                         title={<h3 className="font-semibold m-0">Informations</h3>}
                     >
-                        <ul className="list settings-list">
-                            <li>
-                                <label>{<h4 className="font-semibold m-0">Device name</h4>}</label>
-                                <Input  defaultValue={""} placeholder={"device name"}/>
-                            </li>
-                            <li>
-                                <label>{<h4 className="font-semibold m-0">Description</h4>}</label>
-                                <TextArea showCount maxLength={255} placeholder={"description"} rows={3} defaultValue={""}/>
-                            </li>
-                        </ul>
+                        { status === "loading" ? (<Spin />) : status === "error" ? (error.message) : (
+                            <ul className="list settings-list">
+                                <li>
+                                    <label>{<h4 className="font-semibold m-0">id</h4>}</label>
+                                    <Input disabled={true} defaultValue={data.clientId} />
+                                </li>
+                                <li>
+                                    <label>{<h4 className="font-semibold m-0">Device name</h4>}</label>
+                                    <Input defaultValue={data.name} placeholder={"device name"} />
+                                </li>
+                                <li>
+                                    <label>{<h4 className="font-semibold m-0">Description</h4>}</label>
+                                    <TextArea showCount maxLength={255} placeholder={"description"} rows={3} defaultValue={data.description} />
+                                </li>
+                                <li>
+                                    <label>{<h4 className="font-semibold m-0">Type</h4>}</label>
+                                    <Select defaultValue={DeviceType[1]}>
+                                            {DeviceType.map(type => {
+                                                return (<Select.Option value="type">{type.charAt(0).toUpperCase() + type.slice(1)}</Select.Option>)
+                                            })}
+                                    </Select>
+                                </li>
+                            </ul>
+                        )}
                     </Card>
-                </Col> 
+                </Col>
             </Row>
         </>
     )
