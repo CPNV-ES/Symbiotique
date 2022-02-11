e7c1a0912e45aa0459ec40bed1bcc470885b36f8
SuperQuentin <quentin.aellen@cpnv.ch>
2022-02-11 08:27:11 +0100

    Refactor useDevice hook to use services

---
+++ apps/frontend/src/hooks/devices/useDevice.js
@@ -0,0 +1,12 @@
+import {  useQuery } from 'react-query'
+
+export const getDeviceById = async (deviceId) => {
+    let res = await fetch(`http://localhost:3002/devices/${deviceId}`)
+
+    let data = await res.json()
+    return data;
+}
+
+export const useDevice = (deviceId, options = {}) => {
+    return useQuery(["device", deviceId], () => getDeviceById(deviceId), options);
+}
